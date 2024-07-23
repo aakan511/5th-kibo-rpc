f3c13fde46f92c210cf6a7e10cb6e2cdff6e5688
Aaron Kantsevoy <72467919+aakan511@users.noreply.github.com>
2024-07-22 22:37:54 -0400

    no more moevement improvements found - needs refactor

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/Recognition.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/Recognition.java
@@ -43,33 +43,26 @@ import static org.opencv.objdetect.Objdetect.DICT_5X5_250;
 import static org.opencv.objdetect.Objdetect.getPredefinedDictionary;
 
 public class Recognition implements Runnable{
-//  public String[] classNames;
+  public String[] classNames;
 //  public Net model;
-//  public Context context;
-//  public KiboRpcApi api;
-//  public RecognitionResult[] targets = new RecognitionResult[4];
-//  public ArucoDetector arucoDetector = new ArucoDetector(getPredefinedDictionary(DICT_5X5_250));
-//  public int finalTarget;
-//  public int id;
+  public Context context;
+  public KiboRpcApi api;
+  public RecognitionResult[] targets = new RecognitionResult[4];
+  public ArucoDetector arucoDetector = new ArucoDetector(getPredefinedDictionary(DICT_5X5_250));
+  public int finalTarget;
 
-    private static final String[] CLASS_NAMES = {"beaker", "dropper", "goggle", "hammer",
-            "kapton_tape", "screwdriver", "thermometer", "top", "watch", "wrench"};
+//    private static final String[] CLASS_NAMES = {"beaker", "dropper", "goggle", "hammer",
+//            "kapton_tape", "screwdriver", "thermometer", "top", "watch", "wrench"};
 
     private OrtEnvironment env;
     private OrtSession session;
-    private Context context;
     private int id;
 
-    public Recognition(Context context, int id) {
+    public Recognition(Context context, int id, String[] names, KiboRpcApi api) {
         this.context = context;
         this.id = id;
-        try {
-            env = OrtEnvironment.getEnvironment();
-            String modelPath = Utils.exportResource(context, R.raw.armstrong);
-            session = env.createSession(modelPath, new OrtSession.SessionOptions());
-        } catch(Exception e) {
-            Log.e("Recognition", "Error Loading Model", e);
-        }
+        classNames = names;
+        this.api = api;
     }
 
     public void run() {
@@ -77,7 +70,13 @@ public class Recognition implements Runnable{
     }
 
     public void loadModel(int id) {
-        // needs to be implemented
+        try {
+            env = OrtEnvironment.getEnvironment();
+            String modelPath = Utils.exportResource(context, id);
+            session = env.createSession(modelPath, new OrtSession.SessionOptions());
+        } catch(Exception e) {
+            Log.e("Recognition", "Error Loading Model", e);
+        }
     }
 
     public float[] preprocessImage(Mat image) {
@@ -110,7 +109,7 @@ public class Recognition implements Runnable{
                 int y = (int) (top * originalHeight / 640);
                 int width = (int) ((right - left) * originalWidth / 640);
                 int height = (int) ((bottom - top) * originalHeight / 640);
-                detections.add(new Detection(confidence, new Rect(x, y, width, height), classId, CLASS_NAMES[classId]));
+                detections.add(new Detection(confidence, new Rect(x, y, width, height), classId, classNames[classId]));
             }
         }
 
@@ -168,6 +167,37 @@ public class Recognition implements Runnable{
     }
 
 
+    public RecognitionResult findTarget(Mat image, int id) {
+        int origHeight = image.rows();
+        int origWidth = image.cols();
+
+        float confidenceThreshold = .2f;
+
+        try {
+            float[] inputTensor = preprocessImage(image);
+            float[] results = runInference(inputTensor);
+
+            List<Detection> detections = filterDetections(results, confidenceThreshold, origWidth, origHeight);
+            drawBoundingBoxes(image, detections);
+            api.saveMatImage(image, "recognitionTesting" + id + ".jpg");
+
+            String className = "beaker";
+            int count = detections.size();
+            float maxConfidence = 0.0f;
+
+            for (int i = 0; i < count; i++) {
+                if (detections.get(i).confidence >= maxConfidence) {
+                    maxConfidence = detections.get(i).confidence;
+                    className = detections.get(i).className;
+                }
+            }
+
+            return new RecognitionResult(image, count, className);
+        } catch (Exception e) {
+            Log.e("Recognition", "Identification Error", e);
+        }
+        return new RecognitionResult(image, 0, "beaker");
+    }
 
 //  public Recognition(Context c, int id, String[] names, KiboRpcApi api) {
 //      Log.i("RecognitionDebug", "Recognition constructor began");
@@ -228,36 +258,36 @@ public class Recognition implements Runnable{
 //      int IN_HEIGHT = 640;
 //      double IN_SCALE_FACTOR = 1.0 / 255;
 //      double MEAN_VAL = 0;
-
+//
 //      double yScale = ((double) (img.size().height)) / IN_HEIGHT;
 //      double xScale = ((double) img.size().width) / IN_WIDTH;
-
+//
 //      Log.i("RecognitionTesting", "scales created");
 //      Mat imgRGB = new Mat();
 //      Imgproc.cvtColor(img, imgRGB, Imgproc.COLOR_GRAY2RGB);
-
+//
 //      Log.i("RecognitionTesting", "image converted to color " + img.channels() + ", " + img.type());
 //      Log.i("RecognitionTesting", "color image : " + imgRGB.channels() + ", " + imgRGB.type());
 //      Mat blob = blobFromImage(imgRGB, IN_SCALE_FACTOR, new Size(new Point(IN_WIDTH, IN_HEIGHT)), new Scalar(MEAN_VAL, MEAN_VAL, MEAN_VAL), true, false);
-
+//
 //      Log.i("RecognitionTesting", "created blob");
 //      model.setInput(blob);
 //      Log.i("RecognitionTesting", "set blob as input");
-
+//
 //      Mat outputs = model.forward();
-
+//
 //      Log.i("RecognitionTesting", "Outputs shape : (" + outputs.size().width + " , " + outputs.size().height + ", "+ outputs.channels() + ", " + outputs.total() +")");
 //      Mat mask = outputs.reshape(0, 1).reshape(0, outputs.size(1));
 //      Rect2d[] rect2d = new Rect2d[mask.cols()];
 //      float[] scoref = new float[mask.cols()];
 //      int[] classid = new int[mask.cols()];
-
+//
 //      for (int i = 0; i < mask.cols(); i++) {
 //          double[] x = mask.col(i).get(0, 0);
 //          double[] y = mask.col(i).get(1, 0);
 //          double[] w = mask.col(i).get(2, 0);
 //          double[] h = mask.col(i).get(3, 0);
-
+//
 //          rect2d[i] = new Rect2d((x[0] - w[0]/2) * xScale, (y[0] - h[0]/2) * yScale, w[0] * xScale, h[0] * yScale);
 //          Mat score = mask.col(i).submat(4, outputs.size(1), 0, 1); // outputs.size(1) - 1
 //          Core.MinMaxLocResult mmr = Core.minMaxLoc(score);
@@ -267,17 +297,17 @@ public class Recognition implements Runnable{
 //      MatOfRect2d bboxes = new MatOfRect2d(rect2d);
 //      MatOfFloat scores = new MatOfFloat(scoref);
 //      MatOfInt indeces = new MatOfInt();
-
+//
 //      Dnn.NMSBoxes(bboxes, scores, .49999f, 0.7f, indeces); //nms = .45
 //      Log.i("RecognitionDebug", "indeces total : " + indeces.total());
 //      List<Integer> result = indeces.total() > 0 ? indeces.toList() : new ArrayList<Integer>();
-
+//
 //      int category = 0;
 //      float maxConfidence = 0.0f;
 //      int cnt = result.size();
 //      for (Integer integer : result) {
 //          imgRGB = drawBoundingBox(imgRGB, classid[integer], scoref[integer], rect2d[integer]);
-
+//
 //          if (scoref[integer] > maxConfidence) {
 //              category = classid[integer];
 //              maxConfidence = scoref[integer];
@@ -303,46 +333,43 @@ public class Recognition implements Runnable{
 //      return drawBoundingBox(in, classId, confidence, (int) rect.x, (int) rect.y, (int) (rect.x + rect.width), (int)(rect.y + rect.height));
 //  }
 
-//  public void identify (Mat in){
-//      List<Mat> corners = new ArrayList<>();
-//      Mat ids = new Mat();
+  public void identify (Mat in){
+      List<Mat> corners = new ArrayList<>();
+      Mat ids = new Mat();
 
-//      arucoDetector.detectMarkers(in, corners, ids);
+      arucoDetector.detectMarkers(in, corners, ids);
 
-//      for (int i = 0; i < corners.size(); i++) {
-//          int currTarget = (int) ids.get(i, 0)[0] - 100;
-//          if (currTarget <= 4) {
-//              Mat clean = new Mat();
-//              in.copyTo(clean);
-//              clean = Vision.arucoCrop(clean, corners.get(i));
-//              api.saveMatImage(clean, "target_" + ((int) (ids.get(i, 0)[0] - 100)) + "_" + Vision.randName() + "_cropped.png");
-//              RecognitionResult result = findTarget(clean, currTarget);
-//              //Log.i ("DebuggingTarg4 glitch", "targets[currTarget - 1] : " + currTarget + ", " + finalTarget + ", " + (currTarget == finalTarget));
-//              if (currTarget != 0 && targets[currTarget - 1] == null) {
-//                  api.setAreaInfo(currTarget, result.category, result.numObjects);
-//                  targets[currTarget - 1] = result;
-//              } else if (currTarget == 0){
-//                  api.reportRoundingCompletion();
-//                  for (int j = 0; j < targets.length; j++) {
-//                      if (targets[j] == null) {
-//                          targets[j] = new RecognitionResult(new Mat(), 0, "beaker");
-//                      }
-//                      if (result.category.equals(targets[j].category)) {
-//                          finalTarget = j + 1;
-//                          return;
-//                      }
-//                      if (finalTarget == 4 && targets[j].numObjects == 0 && targets[j].category.equals("beaker")) {
-//                          finalTarget = j + 1;
-//                          Log.i("RECOGNITION_DEBUG", "defaulted to unknown target");
-//                      }
-//                  }
-//              }
-//          }
-
-
-
-//      }
-//  }
+      for (int i = 0; i < corners.size(); i++) {
+          int currTarget = (int) ids.get(i, 0)[0] - 100;
+          if (currTarget <= 4) {
+              Mat clean = new Mat();
+              in.copyTo(clean);
+              clean = Vision.arucoCrop(clean, corners.get(i));
+              api.saveMatImage(clean, "target_" + ((int) (ids.get(i, 0)[0] - 100)) + "_" + Vision.randName() + "_cropped.png");
+              RecognitionResult result = findTarget(clean, currTarget);
+              //Log.i ("DebuggingTarg4 glitch", "targets[currTarget - 1] : " + currTarget + ", " + finalTarget + ", " + (currTarget == finalTarget));
+              if (currTarget != 0 && targets[currTarget - 1] == null) {
+                  api.setAreaInfo(currTarget, result.category, result.numObjects);
+                  targets[currTarget - 1] = result;
+              } else if (currTarget == 0){
+                  api.reportRoundingCompletion();
+                  for (int j = 0; j < targets.length; j++) {
+                      if (targets[j] == null) {
+                          targets[j] = new RecognitionResult(new Mat(), 0, "beaker");
+                      }
+                      if (result.category.equals(targets[j].category)) {
+                          finalTarget = j + 1;
+                          return;
+                      }
+                      if (finalTarget == 4 && targets[j].numObjects == 0 && targets[j].category.equals("beaker")) {
+                          finalTarget = j + 1;
+                          Log.i("RECOGNITION_DEBUG", "defaulted to unknown target");
+                      }
+                  }
+              }
+          }
+      }
+  }
 
 }
 
